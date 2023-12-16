c7fcd81d2e29b306123f44ce2168f0a9eec3bb72
Devyn Spencer <devynspencer@users.noreply.github.com>
2023-12-15 17:16:11 -0800

    Remove unused Lua file

--- media/lua/shared/NoropiaHotkeys.lua
+++
@@ -1,9 +0,0 @@
-NoropiaHotkeys = {
-    keyConfigs = {},
-
-    -- Default options, overwritten by ModOptions on startup
-    Options = {
-        openNotebookByName = true,
-        notebookName = "Field Notes",
-    }
-}
