2885d53d0cd177d6ec20985718a8796f520a2a01
Devyn Spencer <devynspencer@users.noreply.github.com>
2023-12-15 17:17:31 -0800

    Refactor ModOptions integration

--- media/lua/client/NoropiaHotkeys.lua
+++ media/lua/client/NoropiaHotkeys.lua
@@ -2,34 +2,164 @@ require "Options"
 
 NoropiaHotkeys = NoropiaHotkeys or {}
 
--- Add function for plugin behavior
-NoropiaHotkeys.openNotebook = function()
+--- Get the name of the main notebook based on the selected option in the settings.
+--- @return string Name of main notebook
+NoropiaHotkeys.getMainNotebookName = function()
+    local nameOptions = {
+        "My Notes",
+        "My Notebook",
+        "Survivor Notes"
+    }
+
+    return nameOptions[NoropiaHotkeys.settings.mainNotebookName]
+end
+
+--- Get the types of notebook items based on the selected option in the settings.
+--- @return table Table containing the full type name of each item, i.e, Main.Notebook, Main.Journal
+NoropiaHotkeys.getNotebookItemTypes = function()
+    local typeOptions = {
+        "Main.Notebook",
+        "Main.Journal",
+        "Main.Notebook,Main.Journal"
+    }
+
+    local optionString = typeOptions[NoropiaHotkeys.settings.notebookItemTypes]
+    local typeNames = {}
+
+    -- Question: Where the fuck is string.split(',')
+    -- https://stackoverflow.com/questions/19262761/lua-need-to-split-at-comma
+    for typeName in string.gmatch(optionString, '([^,]+)') do
+        typeName = string.gsub(typeName, "%s+", "")
+        table.insert(typeNames, typeName)
+    end
+
+    return typeNames
+end
+
+--- Get the name of the project notebook based on the selected option in settings.
+--- @return string Name of project notebook
+NoropiaHotkeys.getProjectNotebookName = function()
+    -- local value = NoropiaHotkeys.settings.projectNotebookName
+    local nameOptions = {
+        "My Projects",
+        "Projects",
+        "Tasks"
+    }
+
+    return nameOptions[NoropiaHotkeys.settings.projectNotebookName]
+end
+
+--- Get the name of the item list notebook based on selected option in settings.
+--- @return string Name of item list notebook
+NoropiaHotkeys.getItemListNotebookName = function()
+    local nameOptions = {
+        "Items List",
+        "Shopping List",
+        "Materials List",
+        "Loot Tracker",
+        "Wishlist"
+    }
+
+    return nameOptions[NoropiaHotkeys.settings.itemListNotebookName]
+end
+
+--- Get the name of the journal notebook based on selected option in settings.
+--- @return string Name of journal notebook
+NoropiaHotkeys.getJournalNotebookName = function()
+    local nameOptions = {
+        "My Journal",
+        "Journal",
+        "Survivor Journal"
+    }
+
+    return nameOptions[NoropiaHotkeys.settings.journalNotebookName]
+end
+
+--- Open a notebook by name.
+--- @param notebookName string Name of notebook to open
+--- @return nil
+NoropiaHotkeys.openNotebookByName = function(notebookName)
     local player = getPlayer()
     local inventory = player:getInventory()
     local itemsFromFullType = inventory:getItemsFromFullType('Base.Notebook')
+    local notebookFound = false
 
+    -- Iterate through all notebooks in inventory to find the notebook by name
     for i = 0, itemsFromFullType:size() - 1 do
         local current_item = itemsFromFullType:get(i)
         local current_name = current_item:getName()
 
-        if current_name == NoropiaHotkeys.Options.notebookName then
+        if current_name == notebookName then
+            notebookFound = true
+
             ISInventoryPaneContextMenu.onWriteSomething(current_item, true, player:getPlayerNum())
+            -- HaloTextHelper.addTextWithArrow(player, string.format("Ah, here it is! %s", notebookName), true, HaloTextHelper.getColorGreen())
         end
+    end
+
+    -- Handle case where notebook is not found
+    if not notebookFound then
+        player:Say(string.format("I can't find that '%s' notebook anywhere.", notebookName))
     end
 end
 
+--- Open the main notebook.
+--- @return nil
+NoropiaHotkeys.openMainNotebook = function()
+    NoropiaHotkeys.openNotebookByName(NoropiaHotkeys.getMainNotebookName())
+end
+
+--- Open the project notebook.
+--- @return nil
+NoropiaHotkeys.openProjectNotebook = function()
+    NoropiaHotkeys.openNotebookByName(NoropiaHotkeys.getProjectNotebookName())
+end
+
+--- Open the item list notebook.
+--- @return nil
+NoropiaHotkeys.openItemListNotebook = function()
+    NoropiaHotkeys.openNotebookByName(NoropiaHotkeys.getItemListNotebookName())
+end
+
+--- Open the journal notebook.
+--- @return nil
+NoropiaHotkeys.openJournalNotebook = function()
+    NoropiaHotkeys.openNotebookByName(NoropiaHotkeys.getJournalNotebookName())
+end
+
 -- Build keybind configuration for plugin
-local keyConfigs = {
+NoropiaHotkeys.keyConfigs = {
+    openMainNotebook = {
+        isHotAction = false,
+        isFlexKey = false,
+        action = NoropiaHotkeys.openMainNotebook,
+        displayName = "Open player notes.",
+        keyCode = 0
+    },
     openNotebook = {
         isHotAction = false,
-        isFlexKey = true,
-        action = NoropiaHotkeys.openNotebook,
-        displayName = "Open notebook",
+        isFlexKey = false,
+        action = NoropiaHotkeys.openProjectNotebook,
+        displayName = "Open project notes.",
         keyCode = 0
-    }
+    },
+    openItemListNotebook = {
+        isHotAction = false,
+        isFlexKey = false,
+        action = NoropiaHotkeys.openItemListNotebook,
+        displayName = "Open item list notebook.",
+        keyCode = 0
+    },
+    openJournalNotebook = {
+        isHotAction = false,
+        isFlexKey = false,
+        action = NoropiaHotkeys.openJournalNotebook,
+        displayName = "Open player journal",
+        keyCode = 0
+    },
 }
 
 -- Register keybinds as a plugin with EHK
 if EHK_Plugin then
-    EHK_Plugin:AddConfigs(keyConfigs)
+    EHK_Plugin:AddConfigs(NoropiaHotkeys.keyConfigs)
 end
--- media/lua/client/Options.lua
+++ media/lua/client/Options.lua
@@ -1,42 +1,142 @@
-if ModOptions and ModOptions.getInstance then
-    local function applyModOptions(updateData)
-        local newValues = updateData.settings.options
+NoropiaHotkeys = NoropiaHotkeys or {}
+NoropiaHotkeys.settings = NoropiaHotkeys.settings or {}
 
-        NoropiaHotkeys.Options.openNotebookByName = newValues.openNotebookByName
-        NoropiaHotkeys.Options.notebookName = newValues.notebookName
+-- Enable debug mode (prints extra info to console)
+NoropiaHotkeys.settings.debug = false
+
+-- Search for notebook by name, instead of first notebook found.
+NoropiaHotkeys.settings.openNotebookByName = true
+
+-- Full type to use when searching inventory for notebook (Main.Notebook, Main.Journal, Main.Paper, etc.)
+NoropiaHotkeys.settings.notebookItemTypes = 1
+
+-- Name of the main notebook to open
+-- 1 = "My Notes", 2 = "My Notebook", 3 = "Survivor Notes"
+NoropiaHotkeys.settings.mainNotebookName = 1
+
+-- Name of the shopping list notebook to open
+-- 1 = "Shopping List", 2 = "Materials Needed", 3 = "Wishlist"
+NoropiaHotkeys.settings.itemListNotebookName = 1
+
+-- Name of the project notebook to open
+-- 1 = "Projects", 2 = "My Projects", 3 = "Tasks"
+NoropiaHotkeys.settings.projectNotebookName = 1
+
+-- Name of the journal notebook to open
+-- 1 = "My Journal", 2 = "Journal", 3 = "Diary", 4 = "My Diary"
+NoropiaHotkeys.settings.journalNotebookName = 1
+
+-- Append time to new lines in journal
+NoropiaHotkeys.settings.addTimestampToNewLines = true
+
+-- Configure ModOptions
+-- Default values are set above, even if ModOptions isn't installed
+if ModOptions and ModOptions.getInstance then
+    local function onModOptionsApply(optionValues)
+        NoropiaHotkeys.settings.debug = optionValues.settings.options.debug
+        NoropiaHotkeys.settings.openNotebookByName = optionValues.settings.options.openNotebookByName
+        NoropiaHotkeys.settings.notebookItemTypes = optionValues.settings.options.notebookItemTypes
+        NoropiaHotkeys.settings.mainNotebookName = optionValues.settings.options.mainNotebookName
+        NoropiaHotkeys.settings.itemListNotebookName = optionValues.settings.options.itemListNotebookName
+        NoropiaHotkeys.settings.projectNotebookName = optionValues.settings.options.projectNotebookName
+        NoropiaHotkeys.settings.journalNotebookName = optionValues.settings.options.journalNotebookName
+        NoropiaHotkeys.settings.addTimestampToNewLines = optionValues.settings.options.addTimestampToNewLines
+
+        print(string.format("[NoropiaHotkeys] ModOptions settings applied. New settings:\n"))
+
+        for _, option in ipairs(optionValues.settings.options) do
+            print(string.format("[NoropiaHotkeys] %s => [%s]", option.name, tostring(option.value)))
+        end
     end
 
-    -- See ModOptions documentation thread here:
-    -- https://steamcommunity.com/workshop/filedetails/discussion/2169435993/4260919351480715709/
     local SETTINGS = {
         options_data = {
+            -- TODO: Add function (and option) to toggle between notebooks
+            -- TODO: Add function (and option) to toggle notebook UI instead of opening multiples
             openNotebookByName = {
+                name = "Open Notebook By Name",
+                tooltip = "Search for notebooks by name instead of the first notebook found.",
                 default = true,
-                name = "Open notebook by name",
-                tooltip = "Open notebook by name, instead of first notebook found.",
-                OnApplyMainMenu = applyModOptions,
-                OnApplyInGame = applyModOptions,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
             },
-            notebookName = {
-                default = "Field Notes",
-                name = "Notebook name",
-                tooltip = "Notebook name to open, if using 'Open notebook by name' setting.",
-                OnApplyMainMenu = applyModOptions,
-                OnApplyInGame = applyModOptions,
+            -- TODO: Add conditional to openNotebookByName based on NoropiaHotkeys.settings.notebookItemTypes
+            -- TODO: Get actual FullType names from the game
+            -- Full type to use when searching inventory for notebook (Main.Notebook, Main.Journal, Main.Paper, etc.)
+            notebookItemTypes = {
+                "Main.Notebook",
+                "Main.Journal",
+                "Main.Notebook,Main.Journal", -- Base.SheetPaper2
+                name = "Included Item Types",
+                tooltip = "What items are included when searching player inventory for writeable items.",
+                default = 1,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
+            },
+            -- 1 = "My Notes", 2 = "My Notebook", 3 = "Survivor Notes"
+            mainNotebookName = {
+                "My Notes",
+                "My Notebook",
+                "Survivor Notes",
+                name = "Main Notebook Name",
+                tooltip = "Name of the main notebook to open.",
+                default = 1,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
+            },
+            -- 1 = "Shopping List", 2 = "Materials Needed", 3 = "Wishlist"
+            itemListNotebookName = {
+                "Items List",
+                "Shopping List",
+                "Materials List",
+                "Loot Tracker",
+                "Wishlist",
+                name = "Item List Notebook Name",
+                tooltip = "Name of the shopping list notebook to open.",
+                default = 1,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
+            },
+            -- 1 = "Projects", 2 = "My Projects", 3 = "Tasks"
+            projectNotebookName = {
+                "My Projects",
+                "Projects",
+                "Tasks",
+                name = "Project Notebook Name",
+                tooltip = "Name of the project notebook to open.",
+                default = 1,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
+            },
+            -- 1 = "My Journal", 2 = "Journal", 3 = "Diary", 4 = "My Diary"
+            journalNotebookName = {
+                "My Journal",
+                "Journal",
+                "Diary",
+                name = "Daily Journal Notebook Name",
+                tooltip = "Name of the journal notebook to open.",
+                default = 1,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
+            },
+            -- Not yet implemented
+            addTimestampToNewLines = {
+                name = "Add Timestamp to New Entries",
+                tooltip = "Prefix new entries with the current (game) time.",
+                default = true,
+                OnApplyMainMenu = onModOptionsApply,
+                OnApplyInGame = onModOptionsApply
             },
         },
-        mod_id = "NoropiaHotkeys",
-        mod_shortname = "Noropia Hotkeys",
-        mod_fullname = "Noropia Hotkeys"
+        mod_id = 'NoropiaHotkeys',
+        mod_shortname = 'Noropia Hotkeys',
+        mod_fullname = 'Noropia Hotkeys',
     }
 
-    local optionsInstance = ModOptions:getInstance(SETTINGS)
-
+    ModOptions:getInstance(SETTINGS)
     ModOptions:loadFile()
 
-    Events.OnGameStart.Add(
-        function()
-            applyModOptions({ settings = SETTINGS })
-        end
-    )
+    Events.OnGameStart.Add(function()
+        onModOptionsApply({ settings = SETTINGS })
+    end)
 end
